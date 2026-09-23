a00ff7ba3c50de2b80f2be5fef3af71bcd9d8f77
Devansh Manapure <devansh.manapure@gmail.com>
2026-09-23 16:03:55 +0530

    Add files via upload

---
+++ Interest.py
@@ -0,0 +1,6 @@
+#Calculate simple interest
+principle = int(input("Enter Principle:"))
+interest_rate = int(input("Enter Interest:"))
+time = int(input("Enter time"))
+result = principle*interest_rate*time/100
+print("Simple Interest= ",result)
