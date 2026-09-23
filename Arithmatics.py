cfd7ef1395c3790339c91a5ee8cb2ba816b664ca
Devansh Manapure <devansh.manapure@gmail.com>
2026-09-23 15:22:35 +0530

    Add files via upload

---
+++ Arithmatics.py
@@ -0,0 +1,12 @@
+#Perform all arithmetic operations on two numbers
+a = int(input("Enter number a"))
+b = int(input("Enter number b"))
+
+result1 = a - b
+print("Differnce=",result1)
+result2 = a + b
+print("Sum=",result2)
+result3 = a * b
+print("Multiplication=",result3)
+result4 = a / b
+print("Division=",result4)
