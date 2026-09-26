9160dd497ebdcbf91c6313b288558f7f5db7f285
Devansh Manapure <devansh.manapure@gmail.com>
2026-09-26 14:55:24 +0530

    Add files via upload

---
+++ Time.py
@@ -0,0 +1,8 @@
+#Convert seconds into hours, minutes and seconds
+a = int(input("Enter time in seconds"))
+Hours = a/(60*60)
+print("Time in Hours", Hours)
+Minutes = a/60
+print("Timee in Minutes", Minutes)
+Seconds = a
+print("Time in Seconds", Seconds)
